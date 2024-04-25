f7d63d500d375fa79d424ff3f79a59e580666866
HOC HUYNH <hthaihoc2029@gmail.com>
2024-04-25 23:11:27 +0700

    create model Product

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const database = require("./config/database");
+
 require("dotenv").config();
+
+// connect database
+database.connect();
+
+//config router
+const route = require("./routes/client/index.route");
 
 const app = express();
 const port = process.env.PORT;
-
-const route = require("./routes/client/index.route");
 
 app.set("views", "./views");
 app.set("view engine", "pug");
