4334201ede300495ef0e994df5d008575793b370
HOC HUYNH <hthaihoc2029@gmail.com>
2024-04-25 20:56:03 +0700

    add product view

--- index.js
+++ index.js
@@ -1,43 +1,18 @@
 const express = require("express");
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/products-test");
-
 const app = express();
 const port = 3000;
-
-const Product = mongoose.model("Product", {
-  title: String,
-  price: Number,
-  thumbnail: String,
-});
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.use(express.static("public"));
-
 app.get("/", (req, res) => {
-  res.render("index.pug", {
-    title: "Trang chủ",
-    message: "Xin chào các bạn",
-  });
+  res.render("client/pages/home/index");
 });
 
-app.get("/products", async (req, res) => {
-  const products = await Product.find();
-  console.log(products);
-  res.render("products.pug", {
-    title: "San pham",
-    products: products,
-  });
-});
-app.get("/blog", (req, res) => {
-  res.send("<h1>Trang danh sach bai viet</h1>");
-});
-app.get("/contact", (req, res) => {
-  res.send("<h1>Trang lien he</h1>");
+app.get("/products", (req, res) => {
+  res.render("client/pages/products/index");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(port, (req, res) => {
+  console.log(`App listening on port ${port}`);
 });
