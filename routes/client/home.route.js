06f6b6ca04740efee684ed086f407232356c9588
HOC HUYNH <hthaihoc2029@gmail.com>
2024-04-25 21:19:08 +0700

    config controller structure

--- routes/client/home.route.js
+++ routes/client/home.route.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-// const controller = require("../../controllers/client/home.controller");
+const controller = require("../../controllers/client/home.controller");
 
-router.get("/", (req, res) => {
-  res.render("client/pages/home/index");
-});
+router.get("/", controller.index);
 
 module.exports = router;
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("client/pages/products/index");
-});
-router.get("/edit", (req, res) => {
-  res.render("client/pages/products/index");
-});
+const controller = require("../../controllers/client/product.controller");
+
+router.get("/", controller.index);
 
 module.exports = router;
