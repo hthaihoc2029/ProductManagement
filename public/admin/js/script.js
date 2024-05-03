6807d70d7d8eb9d16213920df66c8d2c9cc295df
HOC HUYNH <hthaihoc2029@gmail.com>
2024-05-03 08:33:22 +0700

    complete part 22

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -17,3 +17,21 @@ if (buttonsStatus.length > 0) {
   });
 }
 //End Button Status
+
+//Form Search
+const formSearch = document.querySelector("#form-search");
+if (formSearch) {
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let url = new URL(window.location.href);
+    const keyword = e.target.elements.keyword.value;
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+
+    window.location.href = url.href;
+  });
+}
+//End Form Search
