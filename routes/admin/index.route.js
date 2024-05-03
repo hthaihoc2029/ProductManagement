95e0c00d749541a0e201d2fb6ac0ce2d7a250116
HOC HUYNH <hthaihoc2029@gmail.com>
2024-05-03 07:20:56 +0700

    layout page product manage

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,9 @@
 const systemConfig = require("../../config/system");
 const dashboardRoutes = require("./dashboard.route");
+const productRoutes = require("./product.route");
 
 module.exports = (app) => {
-  app.use(systemConfig.prefixAdmin + "/dashboard", dashboardRoutes);
+  const PATH_ADMIN = systemConfig.prefixAdmin;
+  app.use(PATH_ADMIN + "/dashboard", dashboardRoutes);
+  app.use(PATH_ADMIN + "/products", productRoutes);
 };
