ee72f8a4a94c535640463e94e570582971f66a97
HOC HUYNH <hthaihoc2029@gmail.com>
2024-05-03 09:14:31 +0700

    filter product by title

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -28,11 +28,19 @@ module.exports.index = async (req, res) => {
     find.status = req.query.status;
   }
 
+  let keyword = "";
+  if (req.query.keyword) {
+    keyword = req.query.keyword;
+    const regex = new RegExp(keyword, "i");
+    find.title = regex;
+  }
+
   const products = await Product.find(find);
 
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
+    keyword: keyword,
   });
 };
