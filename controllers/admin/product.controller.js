82cd98bf66f19ca1d461489585f86833eaeb3fba
HOC HUYNH <hthaihoc2029@gmail.com>
2024-05-03 07:32:53 +0700

    get list product from models

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,17 @@
+const Product = require("../../models/product.model");
+
 // [GET] /admin/products
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false,
+  });
+
+  console.log("====================================");
+  console.log(products);
+  console.log("====================================");
+
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
+    products: products,
   });
 };
